8217883abea41a3c66163e8b9b49d8edbe9a88cd
André Felipe do Nascimento Araujo <andre88444@gmail.com>
2026-09-17 10:56:43 -0300

    style: load notification center styles

--- frontend/react/src/main.tsx
+++ frontend/react/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./app/App";
 import "./styles.scss";
+import "./notifications.scss";
 
 createRoot(document.getElementById("root")!).render(<App />);

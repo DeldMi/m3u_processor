50bcd8b2a343205b303baf9e512fc98c0c7f7148
André Felipe do Nascimento Araujo <andre88444@gmail.com>
2026-09-17 17:55:33 -0300

    style: load organized administration styles

--- frontend/react/src/main.tsx
+++ frontend/react/src/main.tsx
@@ -2,5 +2,5 @@ import { createRoot } from "react-dom/client";
 import { App } from "./app/App";
 import "./styles.scss";
 import "./notifications.scss";
-
+import "./organization.scss";
 createRoot(document.getElementById("root")!).render(<App />);
